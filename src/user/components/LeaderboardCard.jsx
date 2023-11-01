1a1746d6c6b6107c5d648ee16bc2e8882873a6ee
NoobiesDoobies <carlioseryan20@gmail.com>
2023-11-01 23:46:17 +0700

    add hover user and update sorting alg to leaderboard page

--- src/user/components/LeaderboardCard.jsx
+++ src/user/components/LeaderboardCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { FaTrophy } from "react-icons/fa6";
 
 import "./LeaderBoardCard.css";
@@ -8,21 +9,23 @@ const LeaderBoardCard = ({ id, name, rank, meetingsAttended }) => {
 
   let rankComponent;
 
-  if(rank === 1){
-    rankComponent = <FaTrophy className="trophy rank-1" />
-  }else if(rank === 2){
-    rankComponent = <FaTrophy className="trophy rank-2" />
-  }else if(rank === 3){
-    rankComponent = <FaTrophy className="trophy rank-3" />
-  }else{
-    rankComponent = <div className="rank">{rank}</div>
+  if (rank === 1) {
+    rankComponent = <FaTrophy className="trophy rank-1" />;
+  } else if (rank === 2) {
+    rankComponent = <FaTrophy className="trophy rank-2" />;
+  } else if (rank === 3) {
+    rankComponent = <FaTrophy className="trophy rank-3" />;
+  } else {
+    rankComponent = <div className="rank">{rank}</div>;
   }
 
   return (
     <li className="leaderboard-card card">
       <div className="leaderboard-card-body card-body">
         {rankComponent}
-        <div>{name}</div>
+        <NavLink to={`/dashboard/${id}`} className="profile-nav-link">
+          {name}
+        </NavLink>
         <div className="total-meeting-attended">{totalMeetingAttended}</div>
       </div>
     </li>
--- src/user/pages/Leaderboard.jsx
+++ src/user/pages/Leaderboard.jsx
@@ -1,43 +1,64 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import LeaderBoardCard from "../components/LeaderboardCard";
 import "./Leaderboard.css";
 
 const Leaderboard = () => {
   const [users, setUsers] = useState([]);
   const [sortedUsers, setSortedUsers] = useState([]);
-  const sortUsersByMeetingsAttended = () => {
-    return users.sort((a, b) => {
+  const [rankList, setRankList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sortUsersAndGenerateIndex = () => {
+    const sortedUsers = users.sort((a, b) => {
       return b.meetingsAttended.length - a.meetingsAttended.length;
     });
+
+    const rankList = [1];
+    let previousValue = 0;
+
+    let j = 1;
+    for (let i = 1; i < sortedUsers.length; i++) {
+      if(sortedUsers[i].meetingsAttended.length !== sortedUsers[i-1].meetingsAttended.length){
+        j++;
+        rankList.push(j)
+        continue
+      }
+      rankList.push(j)
+    }
+    return { sortedUsers, rankList };
   };
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
+        setIsLoading(true);
         const response = await axios.get(`http://localhost:5000/api/users`);
         setUsers(response.data.users);
-        console.log(response.data.users);
+        setIsLoading(false);
       } catch (err) {
-        console.log(err.message);
+        setIsLoading(false);
       }
     };
     fetchUsers();
   }, []);
 
-  useEffect(()=>{
-    setSortedUsers(sortUsersByMeetingsAttended());
-  }, [users])
+  useEffect(() => {
+    setSortedUsers(sortUsersAndGenerateIndex().sortedUsers);
+    setRankList(sortUsersAndGenerateIndex().rankList);
+  }, [users]);
 
   return (
-    <ul className="leaderboard">
-      {sortedUsers.map((user, i ) => {
-        return (
-          <LeaderBoardCard key={user.id} rank={i+1} {...user} />
-        );
-      })}
-    </ul>
+    <>
+      {isLoading && <LoadingSpinner asOverlay />}
+      <ul className="leaderboard">
+        {sortedUsers.map((user, i) => {
+          return <LeaderBoardCard key={user.id} rank={rankList[i]} {...user} />;
+        })}
+      </ul>
+    </>
   );
 };
 
